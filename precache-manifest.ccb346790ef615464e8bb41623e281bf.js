b1c6a19b6af9751adeb8ae00fde557f2834756ad
jhpung0629 <jhpung0629@gmail.com>
2019-11-23 01:50:02 +0900

    update 1.0.0

---
+++ precache-manifest.ccb346790ef615464e8bb41623e281bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa705c53ec77d7bbd295e29a6b65977d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e156d9692d6b6a2c295d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a18fb1138abba19359d6",
+    "url": "/static/css/main.502d9649.chunk.css"
+  },
+  {
+    "revision": "e156d9692d6b6a2c295d",
+    "url": "/static/js/2.348912c4.chunk.js"
+  },
+  {
+    "revision": "a18fb1138abba19359d6",
+    "url": "/static/js/main.2b1feffb.chunk.js"
+  },
+  {
+    "revision": "c638701b8921e36fa11c",
+    "url": "/static/js/runtime-main.36e44473.js"
+  }
+]);
